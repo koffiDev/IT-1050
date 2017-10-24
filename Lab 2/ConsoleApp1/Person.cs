6019cbbccc5878190e35039acdea39cc87a175ef
koffiDev <31811805+koffiDev@users.noreply.github.com>
2017-10-24 18:58:22 -0400

    Lab 2
    Lab 2

---
+++ Lab 2/ConsoleApp1/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Person
+    {
+        public int Age;
+        public string FirstName;
+        public string LastName;
+
+        public Person Spouse;
+
+        public static double SumOfAllAges;
+
+
+        public string GetFullName()
+        {
+            return this.FirstName + ", " + this.LastName;
+        }
+
+
+        public void AskUserForNameAndAge()
+        {
+            System.Console.Write("Your First Name              : ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Your Last Name               : ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.Write("Your Age                     : ");
+            this.Age = int.Parse(System.Console.ReadLine());
+
+            this.Spouse = new Person();
+            System.Console.Write("Your Spouse's First Name     : ");
+            this.Spouse.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("Your Spouse Age              : ");
+            this.Spouse.Age = int.Parse(System.Console.ReadLine());
+
+            this.Spouse.LastName = this.LastName;
+
+            Person.SumOfAllAges += this.Age + this.Spouse.Age;
+        }
+
+        public void PrintNameAndAge()
+        {
+            System.Console.Write(this.GetFullName());
+            System.Console.WriteLine(this.Age);
+            //System.Console.WriteLine(this.Spouse.GetFullName());
+            //System.Console.WriteLine((this.Spouse.Age));
+        }
+  
+        public static double AverageAge()
+        {
+            return (double)Person.SumOfAllAges / 4;
+        }
+    }
+}
---
+++ Lab 2/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("[Enter Information for the First Couple]");
+            System.Console.WriteLine("");
+
+            Person p1 = new Person();
+            p1.AskUserForNameAndAge();
+            
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[Enter Information for the Second Couple]");
+            System.Console.WriteLine("");
+
+            Person p2 = new Person();
+            p2.AskUserForNameAndAge();
+
+            System.Console.WriteLine("");
+            System.Console.WriteLine("[Printing Results]");
+            System.Console.WriteLine("");
+
+            p1.PrintNameAndAge();
+            p1.Spouse.PrintNameAndAge();
+            p2.PrintNameAndAge();
+            p2.Spouse.PrintNameAndAge();
+           
+            System.Console.WriteLine("Average Age = " + Person.AverageAge());
+            System.Console.ReadKey();
+        }
+    }
+}
