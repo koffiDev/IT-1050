63a7e9e0fdbd88cbba91db163920968cae304132
koffiDev <31811805+koffiDev@users.noreply.github.com>
2017-11-09 16:52:44 -0500

    New
    New

---
+++ Lab 3/Lab 3/Instructor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Instructor
+    {
+        private string Name;
+        private string CourseName;
+
+        public Instructor(string Name, string CourseName)
+        {
+            this.Name = Name;
+            this.CourseName = CourseName;
+        }
+
+        public void SetStudentGrade(Student student, int Grade)
+        {
+            student.SetGrade(Grade);
+        }
+
+        public void Print()
+        {
+            System.Console.Write(this.CourseName);
+            System.Console.Write(", which is lecutured by Professor ");
+            System.Console.Write(this.Name);
+            System.Console.Write(".");
+
+        }
+
+    }
+}
---
+++ Lab 3/Lab 3/Student.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab_3
+{
+    class Student
+    {
+        private string Name;
+        private int Grade;
+        private Instructor Teacher; 
+
+        public Student(string Name, Instructor Teacher)
+        {
+            this.Name = Name;
+            this.Grade = 0;
+            this.Teacher = Teacher;
+        }
+        
+        public int SetGrade (int Grade)
+        {
+            return this.Grade = Grade;
+        }
+
+        public void StudentPrint() 
+        {
+            System.Console.Write(this.Name);
+            System.Console.Write(" obtained a grade of ");
+            System.Console.Write(this.Grade);
+            System.Console.Write(" in ");
+            Teacher.Print();
+            System.Console.WriteLine("");
+            System.Console.WriteLine("");
+        }
+    }
+}
